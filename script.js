0dbbddd735afc49cf7016c91d8391d3be7596e5f
muha <1student.muhammed1@gmail.com>
2023-09-08 02:16:02 +0300

    feat:add script for the mouse move and nav when skip the main

---
+++ script.js
@@ -0,0 +1,32 @@
+const nav = document.querySelector('#nav');
+const main = document.querySelector('#main');
+const cursor = document.createElement("div");
+const items = document.querySelectorAll('.item')
+cursor.classList.add("custom-cursor");
+document.body.appendChild(cursor);
+
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.clientX + "px";
+    cursor.style.top = e.clientY + "px";
+});
+
+let isOutsideMain = false;
+
+window.addEventListener('scroll', () => {
+    const mainRect = main.getBoundingClientRect();
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition >= mainRect.bottom && !isOutsideMain) {
+        isOutsideMain = true;
+        nav.style.backgroundColor = '#3333339a';
+        items.forEach(item => {
+            item.style.color = "#F6F4EB"
+        })
+    } else if (scrollPosition < mainRect.bottom && isOutsideMain) {
+        isOutsideMain = false;
+        nav.style.backgroundColor = 'transparent';
+        items.forEach(item => {
+            item.style.color = "#333"
+        })
+    }
+});
